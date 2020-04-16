5378f458abf32d8d5e765ea762da588fd5da55f3
Daniel <daniellimhong@gmail.com>
2020-04-15 21:19:29 -0700

    added chart.js and fixed commit email

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+const data = [
+  {action: "sleep", duration: 8}
+]
+
 function App() {
+  const [agenda, setAgenda] = useState([]);
+
   return (
     <div className="App">
-  
+      
     </div>
   );
 }
